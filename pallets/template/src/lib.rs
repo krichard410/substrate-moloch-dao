5b56c5be654fdc4d5b8d6a41446f54f859e00f5b
Kirsten Richard <kirsten.richard@dal.ca>
2020-06-29 22:47:11 -0300

    Progress made

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -21,6 +21,7 @@ pub trait Trait: system::Trait {
 }
 
 type ProposalIndex = u32;
+type TokenIndex = u32;
 
 #[derive(Encode, Decode, Clone, Default, PartialEq, Eq)]
 pub struct ProposalInfo<AccountId, BlockNumber> {
@@ -31,6 +32,12 @@ pub struct ProposalInfo<AccountId, BlockNumber> {
 	processed: bool,
 	did_pass: bool,
 	aborted: bool,
+}
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+pub struct TokenCreation {
+	tribute_token: u32,
+	payment_token: u32,
 }
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
@@ -45,6 +52,7 @@ decl_event! {
 		Initialized(AccountId),
 		CreateProposal(u32, AccountId),
 		// SubmitVote(proposal_index, sender, unit_vote),
+		CreateToken(u32, u32),
 		Transfer(AccountId, AccountId, u64),
     }
 }
@@ -67,6 +75,10 @@ decl_storage! {
 		Proposals get(fn proposals): map hasher(blake2_128_concat) ProposalIndex => ProposalInfo<T::AccountId, T::BlockNumber>;
 
 		ProposalCount get(fn proposal_count): ProposalIndex;
+
+		Tokens get(fn tokens): map hasher(blake2_128_concat) TokenIndex => TokenCreation;
+
+		TokenCount get(fn token_count): TokenIndex;
 		
 	 	Balances get(fn get_balance): map hasher(blake2_128_concat) T::AccountId => u64;
 
@@ -95,15 +107,15 @@ decl_module! {
 			ensure!(!DidInit::exists(), <Error<T>>::AlreadyInitialized);
 
 			// TODO. Rewrite this part
-			let mut current_memebers = Self::members();
+			let mut current_members = Self::members();
 
 			let new_member = Member {
 				exists: true,
 				highest_index_yes_vote: 0,
 			};
 
-			current_memebers.insert(0, new_member);
-			Members::put(current_memebers);
+			current_members.insert(0, new_member);
+			Members::put(current_members);
 			<Balances<T>>::insert(sender, Self::total_supply());
 			DidInit::put(true);
 		}
@@ -116,18 +128,18 @@ decl_module! {
 			let starting_period;
 			starting_period = <system::Module<T>>::block_number() + T::StartingPeriod::get();
 
-
 			let index = ProposalCount::get();
 			let next_index = index.checked_add(1).ok_or(Error::<T>::Overflow)?;
 			ProposalCount::put(next_index);
+
+			let index_token = TokenCount::get();
+			let following_index = index_token.checked_add(1).ok_or(Error::<T>::Overflow)?;
+			TokenCount::put(following_index);
 
 			let new_proposal = ProposalInfo {
 				proposer: sender.clone(),
 				starting_period: starting_period,
 				// TODO. Write another module for the token creation
-				/// Here the tokens should be another value
-				// tribute_token: sender.clone(), 
-				// payment_token: sender.clone(), 
 				yes_votes: 0,
 				no_votes: 0,
 				processed: false,
@@ -135,8 +147,32 @@ decl_module! {
 				aborted: false,
 			};
 
+			let new_tokens = TokenCreation {
+				tribute_token: 0,
+				payment_token: 0,
+			};
+
+			<Tokens>::insert(index_token, new_tokens);
 			<Proposals<T>>::insert(index, new_proposal);
 			Self::deposit_event(RawEvent::CreateProposal(index, sender));
+			//Self::deposit_event(RawEvent::CreateToken(index, sender));
+		}
+
+		#[weight = 10_000]
+		fn voting_period(origin) {
+			let sender = ensure_signed(origin)?;
+
+			let starting_period = <system::Module<T>>::block_number() + T::StartingPeriod::get();
+
+			let can_vote;
+
+			// TODO. Have the starting period available for only 7 days - BUG HERE
+			if starting_period >= 7 {
+				can_vote = false;
+			}
+			else {
+				can_vote = true;
+			}
 		}
 
 		/*
@@ -146,27 +182,38 @@ decl_module! {
 		*/
 		#[weight = 10_000]
 		fn submit_vote(origin, unit_vote: u8){
-			// let sender = ensure_signed(origin)?;
-			// let proposal_index;
-			// let starting_period = <system::Module<T>>::block_number() + T::StartingPeriod::get();
-			// let voting_expired_period = starting_period + T::VotingPeriod::get();
-			// let mut member = <Members<T>>::get(sender.clone());
+			let sender = ensure_signed(origin)?;
+
+			let starting_period = <system::Module<T>>::block_number() + T::StartingPeriod::get();
+
+			let voting_expired_period = starting_period + T::VotingPeriod::get();
+
+			let proposal_index = ProposalCount::get();
+
+			let member = <Members>::get();
+
+			let proposal = <ProposalInfo>::get();
+
+			// TODO - grab the value of the votes from the struct properly
+			let mut yesVotes = Self::proposal.yes_votes;
+			let mut noVotes = Self::proposal.no_votes;
 
 			// ensure!(unit_vote == 0 || unit_vote == 1, "Vote must be either 0(Yes) or 1(No)");
 
-			// // Vote is counted as yes (0)
-			// if unit_vote == 0 {
-			// 	// Increase value of total yes votes for a specific proposal
-			// 	proposal.yes_votes += 1;
-			// 	if proposal_index >= member.highest_index_yes_vote {
-			// 		member.highest_index_yes_vote = proposal_index;
-			// 	}
-			// // Vote is counted as no (0)
-			// } else {
-			// 	proposal.no_votes += 1;
-			// };
+			// Vote is counted as yes (0)
+			if unit_vote == 0 {
+				// Increase value of total yes votes for a specific proposal
+				// (ProposalInfo.yes_votes : ProposalInfo).yes_votes + 1;
+				yesVotes += 1;
+				// if proposal_index >= member.highest_index_yes_vote {
+				// 	member.highest_index_yes_vote = proposal_index;
+				// }
+			// Vote is counted as no (0)
+			} else {
+				noVotes += 1;
+			};
 
-			// //Self::deposit_event(RawEvent::SubmitVote(proposal_index, sender, unit_vote));
+			//Self::deposit_event(RawEvent::SubmitVote(proposal_index, sender, unit_vote));
 		}
 
 		// if no vote, account take back shares, loss in voting power
@@ -195,18 +242,20 @@ decl_module! {
 			selloffs, or just to top off a stablecoin reserve used to pay for planned expenses
 		*/
 		#[weight = 10_000]
-		fn transfer(_origin, to: T::AccountId, value: u64) {
-			// let sender = ensure_signed(_origin)?;
-			// let sender_balance = Self::get_balance(&sender);
-			// let receiver_balance = Self::get_balance(&to);
+		fn Transfer(_origin, to: T::AccountId, value: u64) {
+			let sender = ensure_signed(_origin)?;
+			let sender_balance = Self::get_balance(&sender);
 
-			// let updated_from_balance = sender_balance.checked_sub(value).ok_or(<Error<T>>::InsufficientFunds)?;
-			// let updated_to_balance = receiver_balance.checked_add(value).expect("Entire supply fits in u64; qed");
+			//Receiver balance reffering to 'guild bank'
+			let receiver_balance = Self::get_balance(&to);
 
-			// <Balances<T>>::insert(&sender, updated_from_balance);
-			// <Balances<T>>::insert(&to, updated_to_balance);
+			let updated_from_balance = sender_balance.checked_sub(value).ok_or(<Error<T>>::InsufficientFunds)?;
+			let updated_to_balance = receiver_balance.checked_add(value).expect("Entire supply fits in u64; qed");
 
-			// Self::deposit_event(RawEvent::Transfer(sender, to, value));
+			<Balances<T>>::insert(&sender, updated_from_balance);
+			<Balances<T>>::insert(&to, updated_to_balance);
+
+			Self::deposit_event(RawEvent::Transfer(sender, to, value));
 		}
     }
 }
--- target/release/wbuild-runner/node-template-runtime9626994530545983703/src/main.rs
+++ target/release/wbuild-runner/node-template-runtime9626994530545983703/src/main.rs
@@ -3,7 +3,7 @@
 
 				fn main() {
 					build_project_with_default_rustflags(
-						"/Users/kirstenrichard/Desktop/substrate-moloch-dao/target/release/build/node-template-runtime-8b6e626665209a05/out/wasm_binary.rs",
+						"/Users/kirstenrichard/Desktop/substrate-moloch-dao/target/release/build/node-template-runtime-315feb5973d97062/out/wasm_binary.rs",
 						"/Users/kirstenrichard/Desktop/substrate-moloch-dao/runtime/Cargo.toml",
 						"-Clink-arg=--export=__heap_base -C link-arg=--import-memory ",
 					)
